ad8e114ec9c7a649926465ba1592f666befb574d
Alena <alena.didenko525@gmail.com>
2021-10-25 00:12:04 +0300

    init commit

---
+++ src/com/pb/didenko/hw3/Array.java
@@ -0,0 +1,13 @@
+package com.pb.didenko.hw3;
+
+import java.util.Random;
+
+public class Array {
+    public static void main(String[] args) {
+    Random random = new Random();
+
+    int x = random.nextInt(5);
+    System.out.println("rand x: " + x);
+
+}
+}
